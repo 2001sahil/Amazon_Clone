ebfbac6d3e66bdff9174b055edb501c5cf7e58f3
2001sahil <114495568+2001sahil@users.noreply.github.com>
2023-05-29 16:02:44 +0530

    may this work

--- App.js
+++ App.js
@@ -15,20 +15,20 @@ app.use(express.json())
 app.use(cors())
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-app.use(express.static(path.join(__dirname, 'public')));
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the index.html file for all routes
-app.get('*', async (req, res) => {
-  try {
-    const response = await fetch('https://raw.githubusercontent.com/2001sahil/Deploy-amazon-clone/<new-branch>/index.html');
-    const html = await response.text();
-    res.send(html);
-  } catch (error) {
-    res.status(500).send('Error retrieving the HTML file.');
-  }
-});
+// // Serve the index.html file for all routes
+// app.get('*', async (req, res) => {
+//   try {
+//     const response = await fetch('https://raw.githubusercontent.com/2001sahil/Deploy-amazon-clone/<new-branch>/index.html');
+//     const html = await response.text();
+//     res.send(html);
+//   } catch (error) {
+//     res.status(500).send('Error retrieving the HTML file.');
+//   }
+// });
 
 
 app.get('/',(req,res)=>{
