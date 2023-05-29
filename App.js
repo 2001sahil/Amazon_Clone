40421e50a0493b2c32cf623eb86c8ec141946eec
2001sahil <114495568+2001sahil@users.noreply.github.com>
2023-05-29 14:59:18 +0530

    checking

--- App.js
+++ App.js
@@ -13,17 +13,17 @@ app.get('/',(req,res)=>{
     res.send("App is live")
 })
 
-app.use(express.static(path.join(__dirname, 'public')));
-// Serves static files for all routes
-app.get('*', async (req, res) => {
-    try {
-      const response = await fetch('https://raw.githubusercontent.com/2001sahil/Deploy-amazon-clone/new-branch/index.html');
-      const html = await response.text();
-      res.send(html);
-    } catch (error) {
-      res.status(500).send('Error retrieving the HTML file.');
-    }
-  });
+// app.use(express.static(path.join(__dirname, 'public')));
+// // Serves static files for all routes
+// app.get('*', async (req, res) => {
+//     try {
+//       const response = await fetch('https://raw.githubusercontent.com/2001sahil/Deploy-amazon-clone/new-branch/index.html');
+//       const html = await response.text();
+//       res.send(html);
+//     } catch (error) {
+//       res.status(500).send('Error retrieving the HTML file.');
+//     }
+//   });
 
 app.use("/Account",require("./LoginANdCreateaccount"))
 app.use("/Cart",require("./Cart"))
